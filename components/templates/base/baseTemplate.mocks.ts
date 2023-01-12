f55781badfde4a72d08bead636bcbf2b418e67b4
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2023-01-13 01:10:59 +0300

    feat: add BaseTemplate component

---
+++ components/templates/base/baseTemplate.mocks.ts
@@ -0,0 +1,11 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+// For using in storybook
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Hello world!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
